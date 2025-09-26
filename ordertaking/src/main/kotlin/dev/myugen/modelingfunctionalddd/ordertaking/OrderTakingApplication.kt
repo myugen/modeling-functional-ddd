a10a8deea195b45cf6219822d42d1a839b36bf70
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2025-09-26 01:42:40 +0100

    chore: include root project package

--- ordertaking/src/main/kotlin/dev/myugen/modelingfunctionalddd/ordertaking/OrderTakingApplication.kt
+++ ordertaking/src/main/kotlin/dev/myugen/modelingfunctionalddd/ordertaking/OrderTakingApplication.kt
@@ -1,4 +1,4 @@
-package dev.myugen.ordertaking
+package dev.myugen.modelingfunctionalddd.ordertaking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class OrderTakingApplication
 
 fun main(args: Array<String>) {
-    runApplication<OrderTakingApplication>(*args)
+  runApplication<OrderTakingApplication>(*args)
 }
