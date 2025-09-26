c46bd01aad5505be7bfcae8282142b4853971116
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2025-09-26 01:13:31 +0100

    initial commit

---
+++ src/main/kotlin/dev/myugen/modelingfunctionalddd/ModelingFunctionalDddApplication.kt
@@ -0,0 +1,11 @@
+package dev.myugen.modelingfunctionalddd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModelingFunctionalDddApplication
+
+fun main(args: Array<String>) {
+  runApplication<ModelingFunctionalDddApplication>(*args)
+}
